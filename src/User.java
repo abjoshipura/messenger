bb717e952587695f7056218ab6d796d9715ae89b
abjoshipura <53368231+abjoshipura@users.noreply.github.com>
2022-11-12 15:58:07 -0500

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,94 +1,67 @@
 import java.util.ArrayList;
-import java.util.Map;
 import java.io.*;
-import java.util.Objects;
 
 public class User {
-    private String name; 
-    private String email; //TODO ensure uniqueness
+    private String username;
+    private String email;
     private String password;
     private ArrayList<User> blockedUsers;
     private ArrayList<User> invisibleUsers;
-
     private boolean requestsCensorship;
-    private Map<String, String> censoredWords;
+    private ArrayList<String> censoredWords;
+    private boolean censorWords;
+    private int numUnreadMessages;
 
-    private boolean hasNewMessages;
+    public User(String userString) {
+        String strippedMessage = userString.substring(userString.indexOf("<") + 1, userString.lastIndexOf(">"));
+        String[] userDetails = strippedMessage.split(", ");
+        this.username = userDetails[0];
+        this.email = userDetails[1];
+        this.password = userDetails[2];
+    }
 
-
-
-    public User(String name, String email, String password) {
-        this.name = name;
+    public User(String username, String email, String password) {
+        this.username = username;
         this.email = email;
         this.password = password;
-        blockedUsers = new ArrayList<>();
-        invisibleUsers = new ArrayList<>();
-        requestsCensorship = false;
-
-        hasNewMessages = false;
-    }
-    /*
-
-    public boolean sendMessageToUser(String message, User user) {
-        try {
-            String fileName = null;
-            String title = "";
-            if(user instanceof Customer) {
-                fileName = user.name.replace(" ", "_") + "&" +
-                        this.name.replace(" ", "_") + ".txt";
-                title = user.name + " & " + this.name;
-            } else {
-                fileName = this.name.replace(" ", "_") + "&" +
-                        user.name.replace(" ", "_") + ".txt";
-                title = this.name + " & " + user.name;
-            }
-            File newMessage = new File(fileName);
-            newMessage.createNewFile();
-
-            FileWriter fileWriter = new FileWriter(fileName);
-            PrintWriter printWriter = new PrintWriter(fileWriter);
-            boolean convoAlreadyExists = false;
-            for(int i = 0; i < AccountHandler.getConversationList().size(); i++) {
-                if(AccountHandler.getConversationList().get(i).getTitle().equals(title)) {
-                    break;
-                }
-            }
-            if(!convoAlreadyExists) {
-                if (this instanceof Customer) {
-                    Conversation conversation = new Conversation(title, fileName, (Customer) this, (Seller) user);
-                } else if (this instanceof Seller) {
-                    Conversation conversation = new Conversation(title, fileName, (Customer) user, (Seller) this);
-                }
-            }
-            Message sentMessage = new Message(message); //Creates new message. String parameter message must be parseable by the constructor
-            printWriter.println(sentMessage + "\n");
-            printWriter.close();
-
-            return true;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
     }
 
-     */
-
-    public boolean sendMessage(String msg, User user) throws IOException {
-        Conversation conversation = AccountHandler.findConversation(user);
-        if (conversation == null) {
-            //todo make new convo
-        } else {
-            BufferedWriter bw = new BufferedWriter(new FileWriter(conversation.getFileName(), true));
-            bw.write(msg.toString());
-            bw.close();
-        }
+    public String toString() {
+        return String.format("User<%s, %s, %s>", this.username, this.email, this.password);
     }
 
-    public boolean sendMessageToConversation(String message, Conversation conversation) {
+    public boolean isParticipantOf(Conversation conversation) {
+        return this.equals(conversation.getSeller()) || this.equals(conversation.getCustomer());
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return username.equals(user.username) && email.equals(user.email) && password.equals(user.password);
+    }
+
+    //TODO Shift to Customer and Seller add Time Stamp
+//    public boolean sendMessageToUser(String message, User user, boolean isDisappearing) {
+    //TODO Handle unique conversation
+    //TODO Store conversation in file | use accountMaster
+
+//        String uniqueConversationID = this.getEmail().remove(".") + "_" + user.getEmail(); //TODO Customer first, buyer next
+//        Conversation conversation = new Conversation(uniqueConversationID, uniqueConversationID + ".txt",
+//                this, user);
+//        AccountsMaster.conversationList.add(conversation);
+//        try {
+//            return conversation.appendToFile(message, this, user, isDisappearing);
+//        } catch (Exception e) {
+//            return false;
+//        }
+//    }
+
+    public boolean sendMessageToConversation(String message, Conversation conversation, boolean isDisappearing) {
+
         try {
             FileWriter fileWriter = new FileWriter(conversation.getFileName(), true);
             PrintWriter printWriter = new PrintWriter(fileWriter);
-            printWriter.println(message + "\n");
+            printWriter.println(message);
             printWriter.close();
             return true;
         } catch (IOException e) {
@@ -97,84 +70,42 @@ public class User {
         }
     }
 
-    public boolean editMessage(String messageID, String newMessage) {
-        ArrayList<Conversation> tempList = AccountHandler.getConversationList();
+    public boolean editMessage(Message message, Conversation conversation, String newMessage) {
         try {
-            Message replace = new Message(newMessage);
-            for (int i = 0; i < tempList.size(); i++) {
-                BufferedReader reader = new BufferedReader(new FileReader(tempList.get(i).getFileName()));
-                FileWriter fileWriter = new FileWriter(tempList.get(i).getFileName(), false);
-                if (tempList.get(i).isParticipant(this)) {
-                    String title = reader.readLine();
-                    String line = reader.readLine();
-                    ArrayList<Message> addMessage = new ArrayList<Message>();
-                    while (line != null) {
-                        // Create message object that is parsed and generated within constructor
-                        Message msg = new Message(line);
-                        addMessage.add(msg);
-                        line = reader.readLine();
+            if (message.getSender().equals(this)) {
+                ArrayList<Message> readMessages = conversation.readFile(this);
+                for (Message readMessage : readMessages) {
+                    if (message.equals(readMessage)) {
+                        readMessage.setMessage(newMessage);
                     }
-                    String theWholeFile = title + "\n";
-                    for(int index = 0; index < addMessage.size(); index++) {
-                        if(addMessage.get(index).getId().equals(messageID)) {
-                            addMessage.set(index, replace);
-                        }
-                        theWholeFile += addMessage.get(index).toString() + "\n";
-
-                    }
-                    fileWriter.write(theWholeFile);
                 }
-                reader.close();
-                fileWriter.flush();
-                fileWriter.close();
+                conversation.writeFile(readMessages);
             }
             return true;
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
-            return false;
-        } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("You cannot edit this message!");
             return false;
         }
     }
 
-    public boolean deleteMessage(String messageID) {
-        ArrayList<Conversation> tempList = AccountHandler.getConversationList();
+    public boolean deleteMessage(Message message, Conversation conversation) {
         try {
-            for (int i = 0; i < tempList.size(); i++) {
-                BufferedReader reader = new BufferedReader(new FileReader(tempList.get(i).getFileName()));
-                FileWriter fileWriter = new FileWriter(tempList.get(i).getFileName(), false);
-                if (tempList.get(i).isParticipant(this)) {
-                    String title = reader.readLine();
-                    String line = reader.readLine();
-                    ArrayList<Message> addMessage = new ArrayList<Message>();
-                    while (line != null) {
-                        Message msg = new Message(line);
-                        addMessage.add(msg);
-                        line = reader.readLine();
+            if (this.isParticipantOf(conversation)) {
+                ArrayList<Message> readMessages = conversation.readFile(this);
+                for (Message readMessage : readMessages) {
+                    if (message.equals(readMessage) && message.getSender().equals(this)) {
+                        readMessage.setSenderVisibility(false);
+                    } else {
+                        readMessage.setRecipientVisibility(false);
                     }
-                    String theWholeFile = title + "\n";
-                    for(int index = 0; index < addMessage.size(); index++) {
-                        if(addMessage.get(index).getId().equals(messageID)) {
-                            addMessage.remove(index);
-                            index--;
-                            continue;
-                        }
-                        theWholeFile += addMessage.get(index).toString() + "\n";
-
-                    }
-                    fileWriter.write(theWholeFile);
                 }
-                reader.close();
-                fileWriter.flush();
-                fileWriter.close();
+                conversation.writeFile(readMessages);
             }
             return true;
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
-            return false;
-        } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("You cannot delete this message!");
             return false;
         }
     }
@@ -187,12 +118,12 @@ public class User {
         invisibleUsers.add(user);
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getEmail() {
@@ -235,30 +166,11 @@ public class User {
         this.requestsCensorship = requestsCensorship;
     }
 
-    public Map<String, String> getCensoredWords() {
+    public ArrayList<String> getCensoredWords() {
         return censoredWords;
     }
 
-    public void setCensoredWords(Map<String, String> censoredWords) {
+    public void setCensoredWords(ArrayList<String> censoredWords) {
         this.censoredWords = censoredWords;
-    }
-    public boolean isHasNewMessages() {
-        return hasNewMessages;
-    }
-
-    public void setHasNewMessages(boolean hasNewMessages) {
-        this.hasNewMessages = hasNewMessages;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof User user)) return false;
-        return isRequestsCensorship() == user.isRequestsCensorship() && isHasNewMessages() == user.isHasNewMessages() && getName().equals(user.getName()) && getEmail().equals(user.getEmail()) && getPassword().equals(user.getPassword()) && Objects.equals(getBlockedUsers(), user.getBlockedUsers()) && Objects.equals(getInvisibleUsers(), user.getInvisibleUsers()) && Objects.equals(getCensoredWords(), user.getCensoredWords());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getName(), getEmail(), getPassword(), getBlockedUsers(), getInvisibleUsers(), isRequestsCensorship(), getCensoredWords(), isHasNewMessages());
     }
 }

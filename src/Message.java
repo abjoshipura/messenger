dde3fcb8c708cf3f40663fe943fcfd46af2f88a6
abjoshipura <53368231+abjoshipura@users.noreply.github.com>
2022-11-12 15:44:16 -0500

    Update Message.java

--- src/Message.java
+++ src/Message.java
@@ -1,23 +1,83 @@
+import java.util.*;
+
 public class Message {
+    private String timeStamp;
+    private final UUID id;
     private String message;
-    private User sender;
-    private User recepient;
-    private String id;
-    private boolean buyerVisibility;
-    private boolean sellerVisibility;
-    private boolean isDisappearing;
+    private final User sender;
+    private final User recipient;
+    private boolean senderVisibility;
+    private boolean recipientVisibility;
 
     public Message(String messageString) {
-        //TODO implement
+        String strippedMessage = messageString.substring(messageString.indexOf("<") + 1,
+                messageString.lastIndexOf(">"));
+
+        String[] messageDetails = strippedMessage.split(", ");
+        this.timeStamp = messageDetails[0];
+        this.id = UUID.fromString(messageDetails[1]);
+
+        int elementsInUserString = 3;
+        int indexOfNextElem = 2;
+        StringBuilder senderString = new StringBuilder();
+        for (int i = indexOfNextElem; i < indexOfNextElem + elementsInUserString; i++) {
+            senderString.append(messageDetails[i]).append(", ");
+        }
+        this.sender = new User(senderString.toString());
+        indexOfNextElem = indexOfNextElem + elementsInUserString;
+
+        StringBuilder recipientString = new StringBuilder();
+        for (int i = indexOfNextElem; i < indexOfNextElem + elementsInUserString; i++) {
+            recipientString.append(messageDetails[i]).append(", ");
+        }
+        this.recipient = new User(recipientString.toString());
+        indexOfNextElem = indexOfNextElem + elementsInUserString;
+
+        this.message = messageDetails[indexOfNextElem++];
+        this.senderVisibility = Boolean.parseBoolean(messageDetails[indexOfNextElem++]);
+        this.recipientVisibility = Boolean.parseBoolean(messageDetails[indexOfNextElem]);
     }
 
-    public Message(String message, User sender, User recepient, boolean isDisappearing, boolean buyerVisibility,
-                   boolean sellerVisibility) {
-        //TODO implement using generateID();
+    public Message(String timeStamp, String message, User sender, User recipient, boolean isDisappearing) {
+        this.timeStamp = timeStamp;
+        this.id = UUID.randomUUID();
+        this.sender = sender;
+        this.recipient = recipient;
+        this.message = message;
+
+        this.senderVisibility = true;
+        this.recipientVisibility = true;
     }
 
-    private String generateID() {
-        return "";
+    public String toString() {
+        return String.format("Message<%s, %s, %s, %s, %s, %b, %b>", this.timeStamp, this.id, this.sender, this.recipient, this.message,
+                this.senderVisibility, this.recipientVisibility);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Message message1)) return false;
+        return senderVisibility == message1.senderVisibility && recipientVisibility == message1.recipientVisibility
+                && id.equals(message1.id) && message.equals(message1.message) && timeStamp.equals(message1.timeStamp)
+                && sender.equals(message1.sender) && recipient.equals(message1.recipient);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    public UUID getID() {
+        return this.id;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public User getRecipient() {
+        return recipient;
     }
 
     public String getMessage() {
@@ -28,31 +88,19 @@ public class Message {
         this.message = message;
     }
 
-    public boolean isBuyerVisibility() {
-        return buyerVisibility;
+    public boolean isSenderVisibility() {
+        return senderVisibility;
     }
 
-    public void setBuyerVisibility(boolean buyerVisibility) {
-        this.buyerVisibility = buyerVisibility;
+    public void setSenderVisibility(boolean senderVisibility) {
+        this.senderVisibility = senderVisibility;
     }
 
-    public boolean isSellerVisibility() {
-        return sellerVisibility;
+    public boolean isRecipientVisibility() {
+        return recipientVisibility;
     }
 
-    public void setSellerVisibility(boolean sellerVisibility) {
-        this.sellerVisibility = sellerVisibility;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String toString() {
-        return id + ";" + sender.getName() + ";" + message;
-    }
-
-    public User getSender() {
-        return sender;
+    public void setRecipientVisibility(boolean recipientVisibility) {
+        this.recipientVisibility = recipientVisibility;
     }
 }

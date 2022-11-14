28e2aa1be6cfa7be84ea5369e3af0e8d854f8d49
yejinoh03 <112606871+yejinoh03@users.noreply.github.com>
2022-11-14 17:39:07 -0500

    Update Seller.java

--- src/Seller.java
+++ src/Seller.java
@@ -26,6 +26,10 @@ public class Seller extends User {
  * according to hasDetails. Additionally, if hasStores, it
  * updates this.stores by calling the Store constructor
  * public Store(String storeString).
+ *
+ * @param sellerString the String for the seller
+ * @param hasDetails whether or not the String should be a detailed version
+ * @param hasStores the property of having stores or not
  */
     public Seller(String sellerString, boolean hasDetails, boolean hasStores) {
         super(sellerString, hasDetails);
@@ -54,6 +58,10 @@ public class Seller extends User {
  * public Seller(String name, String email, String password)
  * Constructor that inherently calls super(String name,
  * String email, String password) to create a new Seller.
+ *
+ * @param name the name of the new seller
+ * @param email the email of the new seller
+ * @param password the password of the new seller
  */
     public Seller(String name, String email, String password) {
         super(name, email, password);
@@ -63,6 +71,8 @@ public class Seller extends User {
     /**
      * public ArrayList<Store> getStores()
      * Returns an ArrayList of Store objects.
+     *
+     * @return the user's list of stores
      */
     public ArrayList<Store> getStores() {
         return this.stores;
@@ -72,6 +82,8 @@ public class Seller extends User {
      * public void addStore(Store store)
      * Adds the Store object passed in to the list of already existing stores for the user.
      * Writes the updated result to the files for later retrieval.
+     *
+     * @param store the store to be added to the user's list
      */
     public void addStore(Store store) {
         String oldUserString;
@@ -89,6 +101,8 @@ public class Seller extends User {
      * public void removeStore(Store store)
      * Removes the Store object passed in from the list of existing stores for the user.
      * Writes the updated result to the files for later retrieval.
+     *
+     * @param store the store to be removed from the user's list
      */
     public void removeStore(Store store) {
         String oldUserString;
@@ -110,6 +124,8 @@ public class Seller extends User {
  * in passwords.txt to store censoring toggle, blocked users,
  * invisible to users, censored word pairs, and owned stores
  * for the next log-in.
+ *
+ * @return formatted String object with more detailed information
  */
     public String detailedToString() {
         return String.format("Seller<%s, %s, %s, %b, %s, %s, %s, %s>", this.getUsername(), this.getEmail(),
@@ -124,6 +140,8 @@ public class Seller extends User {
  * InvisibleUsers, and censoredWords. This is used in
  * writing into a Store object String in passwords.txt
  * to prevent an infinite nested loop of detailedToString() calls.
+ *
+ * @return formatted String object with more details, without store information
  */
     public String detailedToStringWithoutStores() {
         return String.format("Seller<%s, %s, %s, %b, %s, %s, %s>", this.getUsername(), this.getEmail(),
@@ -134,6 +152,8 @@ public class Seller extends User {
     /**
      * public String toString()
      * Returns the formatted String.
+     *
+     * @return formatted String object
      */
     public String toString() {
         return super.toString();
